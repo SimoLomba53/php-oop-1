a9c5ad1c64efac0c33c2340eda67c23735f0bc72
SimoLomba53 <simolomba253@gmail.com>
2023-03-22 16:35:24 +0100

    aggiunta di un metodo

--- index.php
+++ index.php
@@ -1,34 +1,34 @@
 <?php
-  class Movie{
+  
+class Movie {
+
     public $titolo;
     public $regista;
     public $durata;
     public $annodiuscita;
-    public $generation = "WCK";
 
-    public function whatgenis($generation){
-        if ($annodiuscita > 2000){
-             $this->generation = print("NEXT GEN");
+    public function whatgenis(){
+        if ($this->annodiuscita > 2000){
+            return "NEXT GEN";
         }else{
-             $this->generation = print("OLD GEN");
+             return "OLD GEN";
         };
      }
 
-     function __construct($titolo,$regista,$durata,$annodiuscita,$generation) {
-     $this->titolo=$titolo;
-     $this->regista=$regista;
-     $this->durata=$durata;
-     $this->annodiuscita=$annodiuscita;
-     $this->generation=$generation;
+    function __construct($titolo,$regista,$durata,$annodiuscita) {
+        $this->titolo=$titolo;
+        $this->regista=$regista;
+        $this->durata=$durata;
+        $this->annodiuscita=$annodiuscita;
+    }
 
-   }
  };
 
 
-  $pulpfiction=new Movie("Pulp Fiction","Quentin Tarantino","120min",1994,""); 
-  $inception=new Movie("Inception","Christopher Nolan","140min",2010,""); 
+  $pulpfiction=new Movie("Pulp Fiction","Quentin Tarantino","120min",1994); 
+  $inception=new Movie("Inception","Christopher Nolan","140min",2010); 
   
 
-  var_dump($pulpfiction,$inception);
+  var_dump($pulpfiction->generation, $inception->whatgenis());
 
 ?>
