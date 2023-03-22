8c06dd52bd3717735f850041b2dae0b6cb2f4feb
SimoLomba53 <simolomba253@gmail.com>
2023-03-22 16:03:19 +0100

    problema inserimento stringhe

--- index.php
+++ index.php
@@ -7,26 +7,26 @@
     public $generation = "WCK";
 
     public function whatgenis($generation){
-        if ($generation >= 2000){
-            return $this->generation = "NEXT GEN";
+        if ($annodiuscita > 2000){
+             $this->generation = print("NEXT GEN");
         }else{
-            return $this->generation = "OLD GEN";
+             $this->generation = print("OLD GEN");
         };
-     };
+     }
 
      function __construct($titolo,$regista,$durata,$annodiuscita,$generation) {
      $this->titolo=$titolo;
      $this->regista=$regista;
      $this->durata=$durata;
      $this->annodiuscita=$annodiuscita;
-     $this->generation=whatgenis($generation);
+     $this->generation=$generation;
 
-   };
+   }
  };
 
 
-  $pulpfiction=new Movie("Pulp Fiction","Quentin Tarantino","120min",1994); 
-  $inception=new Movie("Inception","Christopher Nolan","140min",2010); 
+  $pulpfiction=new Movie("Pulp Fiction","Quentin Tarantino","120min",1994,""); 
+  $inception=new Movie("Inception","Christopher Nolan","140min",2010,""); 
   
 
   var_dump($pulpfiction,$inception);
