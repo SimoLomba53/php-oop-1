329de88745c74b37dbdffb01c389873929d7e54a
SimoLomba53 <simolomba253@gmail.com>
2023-03-22 16:55:33 +0100

    dati stampati a schermo

--- index.php
+++ index.php
@@ -29,6 +29,30 @@ class Movie {
   $inception=new Movie("Inception","Christopher Nolan","140min",2010); 
   
 
-  var_dump($pulpfiction->generation, $inception->whatgenis());
+  var_dump($pulpfiction,$pulpfiction->whatgenis(),$inception, $inception->whatgenis());
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1><?php echo $pulpfiction->titolo ?></h1>
+        <p><?php echo $pulpfiction->regista ?></p>
+        <p><?php echo $pulpfiction->durata ?></p>
+        <p><?php echo $pulpfiction->annodiuscita ?></p>  
+    </div>
+    <div>
+        <h1><?php echo $inception->titolo ?></h1>
+        <p><?php echo $inception->regista ?></p>
+        <p><?php echo $inception->durata ?></p>
+        <p><?php echo $inception->annodiuscita ?></p>  
+    </div>
+</body>
+</html>
